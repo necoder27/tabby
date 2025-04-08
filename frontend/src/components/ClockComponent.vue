30a038482a1bd06538d8a85ce1fd7dce04eab8d2
necoder27 <laetitiaelbadry@gmail.com>
2025-04-08 21:19:39 +0200

    feat: added clock component

---
+++ frontend/src/components/ClockComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="clock-container">
+    <p id="clock">{{ time }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import {ref} from "vue";
+
+  const time = ref();
+
+  function setCurrentTime() {
+    time.value = new Date().toLocaleTimeString();
+  }
+
+  setInterval(setCurrentTime, 1000)
+</script>
+
+<style scoped>
+  #clock-container {
+    bottom: 0;
+    height: 40%;
+    margin: 4vh 2vw;
+    font-size: 10vh;
+    font-weight: 600;
+  }
+
+  #clock {
+    margin: 0;
+    position: absolute;
+    bottom: 0;
+  }
+</style>
