6125321aafe4d90b8274d83a4839d1435ded5e91
necoder27 <laetitiaelbadry@gmail.com>
2025-04-08 22:22:31 +0200

    feat: added localhost alias and changed config accordingly
    - renamed app

--- backend/api/main.py
+++ backend/api/main.py
@@ -10,7 +10,7 @@ api = FastAPI()
 
 api.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173",],
+    allow_origins=["http://localhost:5173", "http://tabby.dashboard:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
